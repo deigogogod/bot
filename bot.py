653a9c1d966bbd09152fa59d75a6b1c36ff71c50
deigogod <diegoh.duverge@gmail.com>
2025-04-24 17:59:24 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,33 +2,37 @@ import discord
 from bot_logic import gen_pass
 from bot_logic import flip_coin
 from bot_logic import gen_emodji
+from discord.ext import commands
 
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'Hemos iniciado sesión como {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hi! I am a bot')
-    elif message.content.startswith('$smile'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$bye'):
-        await message.channel.send("👋")
-    elif message.content.startswith('$password'):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("hi!")
 
-client.run("token")
+@bot.command()
+async def bye(ctx):
+    await ctx.send("👋")
+
+@bot.command()
+async def pasword(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def smile(ctx):
+    await ctx.send(gen_emodji())
+
+@bot.command()
+async def coin(ctx):
+    await ctx.send(flip_coin())
+
+bot.run("token")
